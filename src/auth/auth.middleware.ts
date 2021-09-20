c365990650476746bc7f2dc349c2982c125cc5f8
hongyiliu <wenhualiu.liu@gmail.com>
2021-09-20 18:03:52 +0800

    add access-control

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -4,6 +4,7 @@ import jwt from 'jsonwebtoken';
 import * as userService from '../user/user.service';
 import { PUBLIC_KEY } from '../app/app.config';
 import { TokenPayload } from './auth.interface';
+import { possess  } from './auth.service'
 export const validateLoginData = async (
   request: Request,
   response: Response,
@@ -48,6 +49,38 @@ export const authGuard = (
     next();
   } catch (error) {
     next(new Error('UNAUTHORIZED'));
+  }
+}
+
+interface AccessControlOptions {
+  possession?: boolean;
+}
+
+export const accessControl = (options: AccessControlOptions) => {
+  return async (request: Request, response: Response, next: NextFunction) => {
+    console.log('👮🏻 访问控制');
+
+    const { possession } = options;
+    const { id: userId } = request.user;
+
+    if (userId == 1) return next();
+
+    const resourceParam = Object.keys(request.params)[0];
+    const resourceType = resourceParam.replace('Id', '');
+    const resourceId = parseInt(request.params[resourceParam], 10);
+
+    if (possession) {
+      try {
+        const ownResource = await possess({ resourceId, resourceType, userId });
+
+        if (ownResource) {
+          return next(new Error('USER_DOES_NOT_OWN_RESOURCE'));
+        }
+      } catch (error) {
+        return next(error)
+      }
+    }
+    next()
   }
 }
 
